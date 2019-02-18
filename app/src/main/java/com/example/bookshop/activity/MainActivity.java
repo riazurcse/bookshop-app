b685b479fae3731f53cbb8d8f849941ef34fb6cd
riaz_cse <riajur.cse90@gmail.com>
2019-02-19 02:30:18 +0600

    app icon added

--- app/src/main/java/com/example/bookshop/activity/MainActivity.java
+++ app/src/main/java/com/example/bookshop/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.bookshop;
+package com.example.bookshop.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.bookshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
